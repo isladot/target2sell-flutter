d3dc648af0af48282966d6151b658233561d797f
Luca Biagetti <islaadot@gmail.com>
2024-03-18 15:04:49 +0100

    feat: example app minor improvements.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,63 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:target2sell_flutter/target2sell_flutter.dart';
+import 'package:target2sell_flutter/target2sell_library.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final target2Sell = Target2Sell(
+    config: Target2SellConfiguration(
+      customerId: '12345678',
+      enableCMP: true,
+    ),
+  );
+
+  await target2Sell.init();
+
   runApp(
-    const App(),
+    App(
+      target2Sell: target2Sell,
+    ),
   );
 }
 
 class App extends StatefulWidget {
   const App({
+    required this.target2Sell,
     super.key,
   });
+
+  final Target2Sell target2Sell;
 
   @override
   State<App> createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  late final Target2Sell target2Sell;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessangerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  late String _rank;
+  late bool _isCMPEnabled;
 
   @override
   void initState() {
-    target2Sell = Target2Sell(
-      config: Target2SellConfiguration(
-        customerId: '12345678',
-        enableCMP: true,
-      ),
-    );
-
-    target2Sell.init();
+    _rank = widget.target2Sell.getRank();
+    _isCMPEnabled = widget.target2Sell.isCMPEnabled;
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Target2Sell Example',
+        scaffoldMessengerKey: _scaffoldMessangerKey,
         home: Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Rank: ${target2Sell.rank}'),
-              ElevatedButton(
-                onPressed: () async {
-                  await target2Sell.sendTracking(
-                    pageId: 1,
-                  );
-                },
-                child: const Text('Send Tracking'),
-              ),
-              ElevatedButton(
-                onPressed: target2Sell.isCMPEnabled
-                    ? () => target2Sell.disableCMP()
-                    : () => target2Sell.enableCMP(),
-                child: target2Sell.isCMPEnabled
-                    ? const Text('Disable CMP')
-                    : const Text('Enable CMP'),
-              ),
-            ],
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Rank: $_rank',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await widget.target2Sell.sendTracking(
+                        trackingRequest: Target2SellTrackingRequest(
+                          pageType: Target2SellPageType.productListingPage,
+                        ),
+                      );
+                    } catch (e) {
+                      _scaffoldMessangerKey.currentState!.showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            e.toString(),
+                            textAlign: TextAlign.center,
+                          ),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Send Tracking'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_isCMPEnabled) {
+                      widget.target2Sell.disableCMP();
+                    } else {
+                      widget.target2Sell.enableCMP();
+                    }
+
+                    setState(() {
+                      _isCMPEnabled = !_isCMPEnabled;
+                    });
+                  },
+                  child: _isCMPEnabled
+                      ? const Text('Disable CMP')
+                      : const Text('Enable CMP'),
+                ),
+              ],
+            ),
           ),
         ),
       );
